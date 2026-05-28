4310cc700cc0f247fc8361d9e82ee90408675f16
Aser IT Service <ammar.eawr@gmail.com>
2026-05-28 12:12:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
 
+// Simple animation on load
+window.onload = () => {
+  console.log("Portfolio Loaded 🚀");
+};
